33bd6a891e539981da52f5277d9c9b20f5ef7e83
terrych322 <acchn@live.com>
2021-12-12 21:58:41 +0800

    feat: 启动页定位

--- web/main.go
+++ web/main.go
@@ -1,11 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(context *gin.Context) {
-		context.Writer.WriteString("hello ihome")
-	})
-	r.Run()
+	router := gin.Default()
+	router.Static("/", "view")
+	//router.StaticFS("/more_static", http.Dir("my_file_system"))
+	//router.StaticFileStaticFS("/more_static", http.Dir("my_file_system"))
+	//router.StaticFile("/favicon.ico", "./resources/favicon.ico")
+
+	// Listen and serve on 0.0.0.0:8080
+	router.Run(":8080")
 }
